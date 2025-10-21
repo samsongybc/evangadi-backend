af0993bb860856a8530a7ca5328ea40f9a75e3da
samsongybc <samsongybc@gmail.com>
2025-10-20 21:22:23 -0400

    Fix dotenv loading for Railway production

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -1,6 +1,40 @@
+// const nodemailer = require("nodemailer");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const sendEmail = async (to, subject, html) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     const info = await transporter.sendMail({
+//       from: `"Forum App" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+
+//     console.log("Email sent:", info.messageId);
+//     return info;
+//   } catch (err) {
+//     console.error("Error sending email:", err);
+//     throw err;
+//   }
+// };
+
+// module.exports = sendEmail;
+
 const nodemailer = require("nodemailer");
-const dotenv = require("dotenv");
-dotenv.config();
+
+// Only load .env in development
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const sendEmail = async (to, subject, html) => {
   try {
@@ -8,7 +42,7 @@ const sendEmail = async (to, subject, html) => {
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, 
+        pass: process.env.EMAIL_PASS,
       },
     });
 
@@ -28,3 +62,54 @@ const sendEmail = async (to, subject, html) => {
 };
 
 module.exports = sendEmail;
+```
+
+---
+
+## **Also: Add Email Variables to Railway**
+
+If your app uses email functionality, you need to add these to Railway → Variables:
+```;
+const nodemailer = require("nodemailer");
+
+// Only load .env in development
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const sendEmail = async (to, subject, html) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: `"Forum App" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("Error sending email:", err);
+    throw err;
+  }
+};
+
+module.exports = sendEmail;
+// ```
+
+// ---
+
+// ## **Also: Add Email Variables to Railway**
+
+// If your app uses email functionality, you need to add these to Railway → Variables:
+// ```;
+// EMAIL_USER=your-gmail@gmail.com
+// EMAIL_PASS=your-app-specific-password
